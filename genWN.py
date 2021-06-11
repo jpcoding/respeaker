b99a0e34c8a8d945571df23ee0ec25600fdb7339
jiaopu94 <jiaopu94@hotmail.com>
2021-06-11 15:56:37 -0500

    audio

--- genWN.py
+++ genWN.py
@@ -4,6 +4,15 @@
 from scipy.io import wavfile
 import numpy as np
 
+def fftnoise(f):
+    f = np.array(f, dtype='complex')
+    Np = (len(f) - 1) // 2
+    phases = np.random.rand(Np) * 2 * np.pi
+    phases = np.cos(phases) + 1j * np.sin(phases)
+    f[1:Np+1] *= phases
+    f[-1:-1-Np:-1] = np.conj(f[1:Np+1])
+    return np.fft.ifft(f).real
+
 def band_limited_noise(min_freq, max_freq, samples=1024, samplerate=1):
     freqs = np.abs(np.fft.fftfreq(samples, 1/samplerate))
     f = np.zeros(samples)
@@ -13,4 +22,4 @@ def band_limited_noise(min_freq, max_freq, samples=1024, samplerate=1):
 
 x = band_limited_noise(20, 20000, 44100, 44100)
 x = np.int16(x * (2**15 - 1))
-wavfile.write("./media/test.wav", 44100, x)
+wavfile.write("test.wav", 44100, x)
